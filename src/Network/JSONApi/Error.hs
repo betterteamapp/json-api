c50df7ce06eb0e21403800fe1dde9ded30198cf1
Ian Duncan <ian@iankduncan.com>
2018-10-18 11:00:53 +0900

    Add source field to Error object

--- src/Network/JSONApi/Error.hs
+++ src/Network/JSONApi/Error.hs
@@ -10,7 +10,7 @@ module Network.JSONApi.Error
 ( Error (..)
 ) where
 
-import Data.Aeson (ToJSON, FromJSON)
+import Data.Aeson (ToJSON, FromJSON, Object)
 import Data.Default
 import Data.Text
 import qualified GHC.Generics as G
@@ -24,27 +24,42 @@ responses.
 
 Specification: <http://jsonapi.org/format/#error-objects>
 -}
-data Error a =
-  Error { id     :: Maybe Text
-        , links  :: Maybe Links
-        , status :: Maybe Text
-        , code   :: Maybe Text
-        , title  :: Maybe Text
-        , detail :: Maybe Text
-        , meta   :: Maybe Meta
-        }
-  deriving (Show, Eq, G.Generic)
+data Error a = Error
+  { id :: Maybe Text
+  -- ^ a unique identifier for this particular occurrence of the problem.
+  , links :: Maybe Links
+  -- ^  a links object containing the following members:
+  --
+  -- [@about@] a link that leads to further details about this particular occurrence of the problem.
+  , status :: Maybe Text
+  -- ^ the HTTP status code applicable to this problem, expressed as a string value.
+  , code :: Maybe Text
+  -- ^ an application-specific error code, expressed as a string value.
+  , title :: Maybe Text
+  -- ^ a short, human-readable summary of the problem that SHOULD NOT change from occurrence to occurrence of the problem, except for purposes of localization.
+  , detail :: Maybe Text
+  -- ^ a human-readable explanation specific to this occurrence of the problem. Like title, this field’s value can be localized.
+  , meta :: Maybe Meta
+  -- ^ a meta object containing non-standard meta-information about the error.
+  , source :: Maybe Object
+  -- ^ source: an object containing references to the source of the error, optionally including any of the following members:
+  --
+  -- [@pointer@] a JSON Pointer [RFC6901] to the associated entity in the request document [e.g. @\/data@ for a primary data object, or @\/data\/attributes\/title@ for a specific attribute].
+  -- [@parameter@] a string indicating which URI query parameter caused the error.
+  } deriving (Show, Eq, G.Generic)
 
 instance ToJSON (Error a)
 instance FromJSON (Error a)
 
 instance Default (Error a) where
-  def = Error
-    { id     = Nothing
-    , links  = Nothing
-    , status = Nothing
-    , code   = Nothing
-    , title  = Nothing
-    , detail = Nothing
-    , meta   = Nothing
-    }
+  def =
+    Error
+      { id = Nothing
+      , links = Nothing
+      , status = Nothing
+      , code = Nothing
+      , title = Nothing
+      , detail = Nothing
+      , meta = Nothing
+      , source = Nothing
+      }
